6cf06e61ddede2cef9e6cc3649cc3f3b5743564a
moayed-mustafa <moayed.abrida@yahoo.com>
2020-07-12 23:03:40 +0400

    solved step1

--- step1.js
+++ step1.js
@@ -1,1 +1,17 @@
-console.log(3)
+
+
+file = require('fs')
+function cat(path) {
+    // read the file
+    file.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.log(`Error reading ${path} \n` ,err)
+            process.exit(1)
+        }
+        console.log(data)
+    })
+}
+
+args = process.argv
+path = args[2]
+cat(path)
